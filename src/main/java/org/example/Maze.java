9b309c127efb75ae1ea55fa862d0702e063e662a
lucas-ribard <lucas.ribard@laplateforme.io>
2023-09-29 15:48:29 +0200

    Fix perfect maze , no hole in unused part of the grid

--- src/main/java/org/example/Maze.java
+++ src/main/java/org/example/Maze.java
@@ -4,13 +4,21 @@ public class Maze {
     public static void main(String[] args) {
         int SizeX = Integer.parseInt(args[0]);
         int SizeY = Integer.parseInt(args[1]);
+
+        if (SizeX < 3 || SizeY < 3){
+            System.out.println("Erreur : Veuillez fournir une largeur et une hauteur valides supérieurs à 3");
+            System.out.println("Utilisation : java -jar MazeRunner.jar [largeur] [hauteur] [perfect/imperfect]");
+            System.exit(1);
+        }
+
         String MazeType = args[2];
 
         if (MazeType.equals("perfect")){
             SimplePerfectMazeGenerator maze = new SimplePerfectMazeGenerator();
             maze.MazeGenerator(SizeX, SizeY);
         }else{
-            System.out.println("MazeType not found");
+            System.out.println("Erreur : Veuillez fournir un type de labyrinthe et une méthode de génération valides.");
+            System.out.println("Utilisation : java -jar MazeRunner.jar [largeur] [hauteur] [perfect/imperfect]");
         }
     }
 }
